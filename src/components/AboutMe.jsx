66bba495e8afdb5a9fba2bd150f4e6c3ca6e96a2
rsm74 <rochellesmenzi@gmail.com>
2026-04-16 11:14:17 +0000

    Update AboutMe.jsx

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -3,7 +3,7 @@ export default function AboutMe() {
     <section id="about" className="m-20 px-10 py-10 rounded-lg shadow-md bg-gradient-to-r from-black via-violet-950 to-black border-b border-violet-900 border-1">
       <h2 className="text-3xl mb-8 text-center text-white font-bold">About Me</h2>
       <div className="flex flex-col md:flex-row items-center md:items-center gap-8 w-full">
-        <img src="/https://drive.google.com/file/d/1vlorFBhGBM1asSvtyUsTbFXimgOIu3DO/view?usp=drive_link" alt="image" className="w-40 h-40 rounded-full object-cover border-2 border-white flex-shrink-0"/>
+        <img src="/2025050710123179.jpg" alt="image" className="w-40 h-40 rounded-full object-cover border-2 border-white flex-shrink-0"/>
         <p className="text-lg text-white leading-relaxed text-center md:text-left">
           Hi, I'm Rochelle S. Menzi, a current student in the University of the Cordilleras in the Bachelor of Science in Information Technology program majoring in Network Security.
           As a student, I am interested in learning and building my skills in web development, particularly interested in designing clean user interfaces through code.
